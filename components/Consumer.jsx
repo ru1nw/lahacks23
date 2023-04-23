3aed358b7ab3a086dfec6e144277967d4a1be654
Ian Wen <h_wen@ucsb.edu>
2023-04-23 09:03:40 -0700

    add 2nd company text

--- components/Consumer.jsx
+++ components/Consumer.jsx
@@ -11,17 +11,21 @@ export function Consumer() {
   let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \
   veniam, quis nostrud exercitation';
+  let company_text = 'Proud inventor of Coal Milk Tea! We heat up our tea by \
+  putting burning hot coal in your drink! We also fly all our plastic cups and \
+  straws from Libya to ensure quality.';
+
   return (
     <>
       <Head>
         <title>Dashboard - SustainABiz</title>
       </Head>
       <div className="company-card-list">
-        <CompanyCard name="First Clothing" goals={lorem} logo={clothes} percent={80} seg={[true, true, false]} />
-        <CompanyCard name="Second-hand Car" goals={lorem} logo={car} percent={80} seg={[false, true, true]} />
-        <CompanyCard name="Third Beauty" goals={lorem} logo={beauty} percent={50} seg={[false, false, true]} />
-        <CompanyCard name="Bach and Fourth" goals={lorem} logo={music} percent={20} seg={[true, false, true]} />
-        <CompanyCard name="Fifth Ave Boba" goals={lorem} logo={boba} percent={20} seg={[false, true, false]} />
+        <CompanyCard name="First Clothing" goals={lorem} logo="" percent={80} seg={[true, true, false]} />
+        <CompanyCard name="Second-hand Car" goals={lorem} logo="" percent={80} seg={[false, true, true]} />
+        <CompanyCard name="Third Beauty" goals={lorem} logo="" percent={50} seg={[false, false, true]} />
+        <CompanyCard name="Bach and Fourth" goals={lorem} logo="" percent={20} seg={[true, false, true]} />
+        <CompanyCard name="Fifth Ave Boba" goals={company_text} logo="" percent={20} seg={[false, true, false]} />
       </div>
     </>
   )
